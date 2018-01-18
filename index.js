7e3ec9230facf7c8b7841ea5b8728c9ea86b7d40
aleclarson <alec.stanford.larson@gmail.com>
2018-01-18 17:51:32 -0500

    improve compatibility with older NodeJS versions

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+'use strict';
 
 // Not all keys can use dot notation.
-const dotNotationRE = /^[a-z_][a-z0-9_]*$/i
+var dotNotationRE = /^[a-z_][a-z0-9_]*$/i;
 
 // Perform strict equality against undefined, by default.
-const defaultCheck = (prop) => prop + ' === undefined'
+var defaultCheck = function defaultCheck(prop) {
+  return prop + ' === undefined';
+};
 
-module.exports =
-function wrapDefaults(defaults, check = defaultCheck) {
-  const setters = Object.keys(defaults).map(key => {
-    const access = dotNotationRE.test(key) ? '.' + key : `['${key}']`
-    const missing = check('props' + access, key)
-    if (!missing) return ''
-    return `\n  if (${missing}) props${access} = defaults${access}`
-  }).join('')
-  return new Function('defaults', `return function(props) {${setters}\n  return props\n}`)(defaults)
-}
+module.exports = function wrapDefaults(defaults) {
+  var check = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultCheck;
+
+  var setters = Object.keys(defaults).map(function (key) {
+    var access = dotNotationRE.test(key) ? '.' + key : '[\'' + key + '\']';
+    var missing = check('props' + access, key);
+    if (!missing) return '';
+    return '\n  if (' + missing + ') props' + access + ' = defaults' + access;
+  }).join('');
+  return new Function('defaults', 'return function(props) {' + setters + '\n  return props\n}')(defaults);
+};
